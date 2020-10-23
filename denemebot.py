4ae2cea7090928c56fcad5630997ff3f42c3f5c7
emrekiraz <61597816+emrekiraz@users.noreply.github.com>
2020-10-23 03:21:19 +0300

    Update denemebot.py

--- denemebot.py
+++ denemebot.py
@@ -2,7 +2,31 @@ import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
 import random
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import sys
+import string
+from random import *
 import os
+
+characters = string.ascii_letters + string.punctuation  + string.digits
+password =  "".join(choice(characters) for x in range(randint(7, 14)))
+
+
+mail = smtplib.SMTP("smtp.gmail.com",587)
+mail.ehlo()
+mail.starttls()
+mail.login("marselinhani@gmail.com", os.environ.get('sifre'))
+
+mesaj = MIMEMultipart()
+mesaj["From"] = "marselinhani@gmail.com"      # Gönderen
+mesaj["Subject"] = "Python Smtp ile Mail Gönderme"    # Konusu
+
+body = "Parolanız: " + password
+
+body_text = MIMEText(body, "plain")  #
+mesaj.attach(body_text)
 
 bot = discord.Client()
 bot_prefix = "!"
@@ -14,11 +38,27 @@ async def on_ready():
     print("İsim: {}".format(bot.user.name))
     print("ID: {}".format(bot.user.id))
     print(str(len(set(bot.get_all_members()))) + " tane üye aktif.")
-    await bot.change_presence(game=discord.Game(name="Compec'e Öğrenci Topluyor..."))
+    await bot.change_presence(activity=discord.Game(name="Compec'e Öğrenci Topluyor..."))
 
 @bot.command(pass_context=True)
 async def posta(ctx):
     await bot.say("Ben CompecMan!")
+
+@bot.command(pass_context=True)
+async def eposta(ctx, epostaadr):
+    epostaadr = str(epostaadr)
+    mesaj["To"] = epostaadr
+    mail.sendmail(mesaj["From"], mesaj["To"], mesaj.as_string())
+    print("Mail başarılı bir şekilde gönderildi.")
+    mail.close()
+
+@bot.command(pass_context=True)
+async def dogrula(ctx, dogrulakod):
+    dogrulakod = dogrulakod
+    if dogrulakod == password:
+        await bot.say("Hesabın doğrulandı!")
+    else:
+        await bot.say("Tekrar Dene!")
 
 @bot.command(pass_context=True)
 async def a(ctx, member : discord.Member):
@@ -35,6 +75,6 @@ async def sil (ctx, number):
     number = int(number)
     async for x in bot.logs_from(ctx.message.channel, limit=number):
         mgs.append(x)
-    await bot.delete_messages(mgs)
+    await ctx.delete_messages(mgs)
 
 bot.run(os.environ.get('token'))
