ca6f694cbeb7ecfa9c33447a698b15c5632679f5
emrekiraz <61597816+emrekiraz@users.noreply.github.com>
2020-10-23 03:58:27 +0300

    Update denemebot.py

--- denemebot.py
+++ denemebot.py
@@ -2,7 +2,31 @@ import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
 import random
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import sys
+import string
+from random import *
 import os
+
+characters = string.ascii_letters + string.punctuation  + string.digits
+password =  "".join(choice(characters) for x in range(randint(7, 14)))
+
+
+mail = smtplib.SMTP("smtp.gmail.com",587)
+mail.ehlo()
+mail.starttls()
+mail.login("marselinhani@gmail.com", os.environ.get('sifre'))
+
+mesaj = MIMEMultipart()
+mesaj["From"] = "marselinhani@gmail.com"      # Gönderen
+mesaj["Subject"] = "Python Smtp ile Mail Gönderme"    # Konusu
+
+body = "Parolanız: " + password
+
+body_text = MIMEText(body, "plain")  #
+mesaj.attach(body_text)
 
 bot = discord.Client()
 bot_prefix = "!"
@@ -19,6 +43,22 @@ async def on_ready():
 @bot.command(pass_context=True)
 async def posta(ctx):
     await bot.say("Ben CompecMan!")
+
+@bot.command(pass_context=True)
+async def eposta(ctx, epostaadr):
+    epostaadr = str(epostaadr)
+    mesaj["To"] = epostaadr
+    mail.sendmail(mesaj["From"], mesaj["To"], mesaj.as_string())
+    print("Mail başarılı bir şekilde gönderildi.")
+    mail.close()
+
+@bot.command(pass_context=True)
+async def dogrula(ctx, dogrulakod):
+    dogrulakod = dogrulakod
+    if dogrulakod == password:
+        await bot.say("Hesabın doğrulandı!")
+    else:
+        await bot.say("Tekrar Dene!")
 
 @bot.command(pass_context=True)
 async def a(ctx, member : discord.Member):
