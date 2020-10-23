7e68869e5c5365bfa24607cf74e6510da3b545b7
emrekiraz <61597816+emrekiraz@users.noreply.github.com>
2020-10-23 03:56:49 +0300

    Update denemebot.py

--- denemebot.py
+++ denemebot.py
@@ -1,7 +1,12 @@
+import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
 import random
 import os
+
+bot = discord.Client()
+bot_prefix = "!"
+bot = commands.Bot(command_prefix=bot_prefix)
 
 @bot.event
 async def on_ready():
